9314a7cc5278f30e9b4a8e1d69decfc61cf63850
Gianfranco <culicchia.gianfranco@gmail.com>
2020-03-29 22:42:11 +0200

    menage

---
+++ src/test/java/com/gigi/toc/TocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigi.toc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
